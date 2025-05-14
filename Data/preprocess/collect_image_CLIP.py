db9bea49a478138bf5dba9011f4fe5359432f821
Ivan <83827643+justivanr@users.noreply.github.com>
2025-05-14 09:22:21 +0200

    Update collect_image_CLIP.py

--- Data/preprocess/collect_image_CLIP.py
+++ Data/preprocess/collect_image_CLIP.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+modules_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
 sys.path.append(modules_dir)
 from im2wav_utils import *
 from Data.meta import ImageHear_paths
